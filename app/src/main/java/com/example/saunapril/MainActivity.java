b39eb77a360d7a52d1f9dbeccc024e3b81063100
Graiffy <graiffydesign@gmail.com>
2026-03-11 17:00:13 +0300

    Feat: рабочее меню с тостами

--- app/src/main/java/com/example/saunapril/MainActivity.java
+++ app/src/main/java/com/example/saunapril/MainActivity.java
@@ -1,15 +1,16 @@
 package com.example.saunapril;
-
 import android.os.Bundle;
-import android.view.View;
-
+import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,17 +18,37 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        drawerLayout = findViewById(R.id.drawerLayout);
+        NavigationView navigationView = findViewById(R.id.navigationView);
+
+        findViewById(R.id.btnMenu).setOnClickListener(v ->
+                drawerLayout.openDrawer(GravityCompat.END)
+        );
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+
+            if (id == R.id.nav_login) {
+                Toast.makeText(this, "Войти/Выйти", Toast.LENGTH_SHORT).show();
+            } else if (id == R.id.nav_edit_halls) {
+                Toast.makeText(this, "Редактирование залов", Toast.LENGTH_SHORT).show();
+            } else if (id == R.id.nav_edit_bookings) {
+                Toast.makeText(this, "Редактирование броней", Toast.LENGTH_SHORT).show();
+            }
+
+            drawerLayout.closeDrawer(GravityCompat.END);
+            return true;
         });
 
-
-        findViewById(R.id.btnMenu).setOnClickListener(new View.OnClickListener() {
+        getOnBackPressedDispatcher().addCallback(this, new androidx.activity.OnBackPressedCallback(true) {
             @Override
-            public void onClick(View v) {
-                // Тут позже сделаем открытие меню
+            public void handleOnBackPressed() {
+                if (drawerLayout.isDrawerOpen(GravityCompat.END)) {
+                    drawerLayout.closeDrawer(GravityCompat.END);
+                } else {
+                    setEnabled(false);
+                    getOnBackPressedDispatcher().onBackPressed();
+                }
             }
         });
     }
