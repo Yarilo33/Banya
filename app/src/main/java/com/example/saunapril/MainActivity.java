1d1509dd6810bab63a8d34492b80cb176e8aaac1
Graiffy <graiffydesign@gmail.com>
2026-03-11 16:38:04 +0300

    Feat: строка поиска зала и заготовка под меню

--- app/src/main/java/com/example/saunapril/MainActivity.java
+++ app/src/main/java/com/example/saunapril/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.saunapril;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +16,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+
+        findViewById(R.id.btnMenu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Тут позже сделаем открытие меню
+            }
         });
     }
 }
